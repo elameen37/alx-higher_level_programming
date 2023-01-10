59e1f26f9295251089e4eed023093c2b20159f56
elameen37 <elameen37@gmail.com>
2023-01-10 02:00:54 +0100

    update task

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,5 +5,4 @@ class MyList(list):
     """Type class MyList with print_sorted function"""
 
     def print_sorted(self):
-		"""Print a list in sorted ascending order."""
         print(sorted(self))
